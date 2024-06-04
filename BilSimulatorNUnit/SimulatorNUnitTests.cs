aee901042b118166874f959587cbbfba6286ed00
hmacker <chammas_mousa@live.se>
2024-06-04 19:13:40 +0200

    Added NUnit project and TestCases for drive

---
+++ BilSimulatorNUnit/SimulatorNUnitTests.cs
@@ -0,0 +1,35 @@
+﻿using BilSimulator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilSimulatorNUnit
+{
+    [TestFixture]
+    public class SimulatorNUnitTests
+    {
+        [SetUp]
+        public void setup()
+        {
+            CarActions.IsTesting = true;
+            DriverActions.IsTesting = true;
+            Status.IsTesting = true;
+        }
+
+        [TestCase(100, 90, 10)]
+        [TestCase(50, 40, 10)]
+        [TestCase(10, 0, 10)]
+        public void TestDrive(int initialFuel, int expectedFuel, int expectedTiredness)
+        {
+            var car = new Car { Fuel = initialFuel, Direction = "Norrut" };
+            var driver = new Driver { Tiredness = 0 };
+
+            CarActions.Drive(car, driver);
+
+            Assert.AreEqual(expectedFuel, car.Fuel);
+            Assert.AreEqual(expectedTiredness, driver.Tiredness);
+        }
+    }
+}
