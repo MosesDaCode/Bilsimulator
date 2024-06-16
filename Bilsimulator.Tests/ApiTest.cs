315f9b4e1e67c727b36f26a64b9f10893d4c923b
hmacker <chammas_mousa@live.se>
2024-06-16 17:23:36 +0200

    Added TestMethod to test Api

--- Bilsimulator.Tests/ApiTest.cs
+++ Bilsimulator.Tests/ApiTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Services.ApiService;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,31 @@ using System.Threading.Tasks;
 
 namespace BilSimulator.Tests
 {
+    [TestClass]
     public class ApiTest
     {
+        private HttpClient _httpClient;
+        private GetTheApi _getTheApi;
 
+        [TestInitialize]
+        public void Setup()
+        {
+            _httpClient = new HttpClient();
+            _getTheApi = new GetTheApi(_httpClient);
+        }
+
+        [TestMethod]
+        public async Task FetchApidata_ShouldReturnRandomUser()
+        {
+            //ACT
+            var result = await _getTheApi.FetchApiData();
+
+            //ASSERT
+            Assert.IsNotNull(result);
+            Assert.IsFalse(string.IsNullOrEmpty(result.First));
+            Assert.IsFalse(string.IsNullOrEmpty(result.Last));
+            Assert.IsFalse(string.IsNullOrEmpty(result.Title));
+
+        }
     }
 }
